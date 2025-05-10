b77e3011f18e83ff8994dd64eceb39448fc07ee9
Danny Xie <51223285+dnnxl@users.noreply.github.com>
2025-05-10 11:02:41 -0600

    add perceptual loss file

--- perceptual_loss.py
+++ perceptual_loss.py
@@ -1,59 +1,103 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torchvision
+from torchvision import models
+import copy
 
-class PerceptualLossVGG(nn.Module):
-    """VGG/Perceptual Loss
-    
-    Parameters
-    ----------
-    conv_index : str
-        Convolutional layer in VGG model to use as perceptual output
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    """
-    def __init__(self, conv_index: str = '22'):
+# Helper: Normalize for VGG
+imagenet_mean = torch.tensor([0.485, 0.456, 0.406]).to(device).view(1, 3, 1, 1)
+imagenet_std = torch.tensor([0.229, 0.224, 0.225]).to(device).view(1, 3, 1, 1)
 
-        super(VGG, self).__init__()
-        vgg_features = torchvision.models.vgg19(pretrained=True).features
-        modules = [m for m in vgg_features]
+def normalize_for_vgg(x):
+    return (x - imagenet_mean) / imagenet_std
+
+# Helper: Gram Matrix for style loss
+def gram_matrix(tensor):
+    b, c, h, w = tensor.size()
+    features = tensor.view(b, c, h * w)
+    G = torch.bmm(features, features.transpose(1, 2))
+    return G #/ (c * h * w)
+
+# VGG feature extractor builder
+def build_vgg_perceptual_model(layers=None):
+    if layers is None:
+        layers = ['relu_13', 'relu_14', 'relu_15', 'relu_16']
         
-        if conv_index == '22':
-            self.vgg = nn.Sequential(*modules[:8])
-        elif conv_index == '54':
-            self.vgg = nn.Sequential(*modules[:35])
+    vgg = models.vgg19(pretrained=True).features.to(device).eval()
+    for param in vgg.parameters():
+        param.requires_grad = False
 
-        vgg_mean = (0.485, 0.456, 0.406)
-        vgg_std = (0.229, 0.224, 0.225)
-        #self.sub_mean = common.MeanShift(rgb_range, vgg_mean, vgg_std)
-        self.vgg.requires_grad = False
+    selected_layers = {}
+    model = nn.Sequential().to(device)
+    relu_count = 0
+    conv_count = 0
 
-    def forward(self, sr: torch.Tensor, hr: torch.Tensor) -> torch.Tensor:
-        """Compute VGG/Perceptual loss between Super-Resolved and High-Resolution
+    for layer in vgg.children():
+        if isinstance(layer, nn.Conv2d):
+            conv_count += 1
+            name = f'conv_{conv_count}'
+        elif isinstance(layer, nn.ReLU):
+            relu_count += 1
+            name = f'relu_{relu_count}'
+            layer = nn.ReLU(inplace=False)
+        elif isinstance(layer, nn.MaxPool2d):
+            name = f'pool_{relu_count}'
+        else:
+            continue
 
-        Parameters
-        ----------
-        sr : torch.Tensor
-            Super-Resolved model output tensor
-        hr : torch.Tensor
-            High-Resolution image tensor
+        model.add_module(name, layer)
+        if name in layers:
+            selected_layers[name] = copy.deepcopy(model)
+        if len(selected_layers) == len(layers):
+            break
 
-        Returns
-        -------
-        loss : torch.Tensor
-            Perceptual VGG loss between sr and hr
+    return selected_layers
 
-        """
-        def _forward(x):
-            #x = self.sub_mean(x)
-            x = self.vgg(x)
-            return x
-            
-        vgg_sr = _forward(sr)
+# Combined perceptual loss with content + style + KL
+def vae_perceptual_loss(reconstructed, original, mean, logvar, perceptual_model, kl_beta=0.1,
+                        content_layers=['relu_2', 'relu_3', 'relu_5'], style_layers=['relu_14', 'relu_15', 'relu_16'],
+                        style_weight=0.0, content_weight=1.0):
+    b_size = reconstructed.size(0)
+    loss_fn = nn.MSELoss(reduction='mean')
 
+    # Normalize inputs for VGG
+    original = normalize_for_vgg(original.to(device))
+    reconstructed = normalize_for_vgg(reconstructed.to(device))
+
+    # Feature extraction
+    feats_orig = {}
+    feats_recon = {}
+    for name, submodel in perceptual_model.items():
         with torch.no_grad():
-            vgg_hr = _forward(hr.detach())
+            feats_orig[name] = submodel(original).detach()
+        feats_recon[name] = submodel(reconstructed)
 
-        loss = F.mse_loss(vgg_sr, vgg_hr)
+    # Content loss from multiple layers
+    content_loss = 0.0
+    for layer in content_layers:
+        b, c, h, w = feats_recon[layer].size()
+        content_loss += loss_fn(feats_recon[layer], feats_orig[layer]) #/ c #/ (c * h * w)
+        
+    # Style loss
+    style_loss = 0.0
+    for layer in style_layers:
+        G1 = gram_matrix(feats_orig[layer])
+        G2 = gram_matrix(feats_recon[layer])
+        style_loss += loss_fn(G1, G2)
 
-        return loss
+    # Pixel-space reconstruction loss
+    pixel_recon_loss = loss_fn(reconstructed, original)
+
+    # KL divergence
+    kl_div = -torch.mean(1 + logvar - mean.pow(2) - logvar.exp())
+    
+    content_loss = content_weight * content_loss
+    style_loss = style_weight * style_loss
+    kl_div = kl_beta * kl_div
+    # Weighted total loss
+    total_loss = pixel_recon_loss + content_loss + style_loss + kl_div
+    
+    
+    return total_loss / b_size, pixel_recon_loss / b_size, content_loss / b_size, style_loss / b_size, kl_div / b_size
